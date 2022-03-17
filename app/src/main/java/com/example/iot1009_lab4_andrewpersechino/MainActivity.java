bb531ff302afd18daf7e8341d907b7159f035f65
Andrew <andrewdp56@outlook.com>
2022-03-17 13:31:44 -0400

    Initial commit

---
+++ app/src/main/java/com/example/iot1009_lab4_andrewpersechino/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.iot1009_lab4_andrewpersechino;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

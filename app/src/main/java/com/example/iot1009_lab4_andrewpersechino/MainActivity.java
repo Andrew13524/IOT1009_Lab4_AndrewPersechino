59017b7cab8d0b73f952e88d07afb76b132810c7
Andrew <andrewdp56@outlook.com>
2022-03-17 14:02:25 -0400

    Added UI Elements

--- app/src/main/java/com/example/iot1009_lab4_andrewpersechino/MainActivity.java
+++ app/src/main/java/com/example/iot1009_lab4_andrewpersechino/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.iot1009_lab4_andrewpersechino;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> arrayList;
+    EditText editText;
+    ListView listView;
+    Button addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        arrayList = new ArrayList<>();
+        editText = findViewById(R.id.editText);
+        listView = findViewById(R.id.listView);
+        addButton = findViewById(R.id.addButton);
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.select_dialog_multichoice, arrayList);
+        listView.setAdapter(arrayAdapter);
+
     }
 }

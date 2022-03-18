1825f0a71429875a795b2897e7c78c871c8597e0
Andrew <andrewdp56@outlook.com>
2022-03-18 17:21:09 -0400

    Added app functionality

--- app/src/main/java/com/example/iot1009_lab4_andrewpersechino/MainActivity.java
+++ app/src/main/java/com/example/iot1009_lab4_andrewpersechino/MainActivity.java
@@ -3,14 +3,17 @@ package com.example.iot1009_lab4_andrewpersechino;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckedTextView;
 import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, View.OnClickListener {
     ArrayList<String> arrayList;
     EditText editText;
     ListView listView;
@@ -21,13 +24,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Setting default values
         arrayList = new ArrayList<>();
         editText = findViewById(R.id.editText);
         listView = findViewById(R.id.listView);
         addButton = findViewById(R.id.addButton);
-
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.select_dialog_multichoice, arrayList);
         listView.setAdapter(arrayAdapter);
 
+        // Setting listeners
+        listView.setOnItemClickListener(this);
+        addButton.setOnClickListener(this);
+    }
+
+    // Add text to list, and clear editText
+    @Override
+    public void onClick(View view) {
+        arrayList.add(editText.getText().toString());
+        editText.setText("");
+    }
+
+    // Change check value on List
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        CheckedTextView v = (CheckedTextView) view;
+        v.setChecked(!v.isChecked());
     }
 }
